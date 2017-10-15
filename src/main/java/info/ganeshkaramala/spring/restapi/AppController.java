1f2de4585d3f2fa398ca2c0b2d854b1961f0cf2d
Ganesh Karamala <ganeshkaramala@gmail.com>
2017-10-15 22:46:55 +0530

    first commit
    Signed-off-by: Ganesh Karamala <ganeshkaramala@gmail.com>

---
+++ src/main/java/info/ganeshkaramala/spring/restapi/AppController.java
@@ -0,0 +1,27 @@
+package info.ganeshkaramala.spring.restapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AppController {
+	
+	@RequestMapping(value = "/", method = { RequestMethod.GET,
+			RequestMethod.POST })
+	public ResponseEntity<String> welcomeGET(
+			@RequestParam(required = false, value = "name") String name) {
+
+		String welcome = "";
+		if (name != null && name.length() > 0) {
+			welcome = "<h1>Welcome " + name + "!</h1>";
+		} else {
+			welcome = "<h1>Welcome!</h1>";
+		}
+
+		return new ResponseEntity<String>(welcome, HttpStatus.OK);
+	}
+}
